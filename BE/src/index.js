90d52219f5e9a615bb66dab9dcd4eeab578ba015
tnaw <tuannam5604@gmail.com>
2025-10-30 19:02:26 +0700

    fix titlle

--- BE/src/index.js
+++ BE/src/index.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { loadSensorMapping } = require('./controllers/sensor.controller');
-// Chỉ cần require file này là MQTT client sẽ tự động chạy
-require('./mqtt/mqttHandler'); // Kích hoạt lại MQTT Handler
+
+require('./mqtt/mqttHandler'); 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Hàm main để khởi tạo các thành phần bất đồng bộ
+
 async function main() {
-  // Tải sensor mapping từ CSDL trước khi khởi động server
+  
   await loadSensorMapping();
 
   app.listen(port, () => {
@@ -18,22 +18,20 @@ async function main() {
   });
 }
 
-// Import các routes
+
 const sensorRoutes = require('./routes/sensor.routes');
 const deviceRoutes = require('./routes/device.routes');
-const userRoutes = require('./routes/user.routes'); // Thêm route cho user
+const userRoutes = require('./routes/user.routes'); 
 
 
-// Middlewares
+
 app.use(cors());
 app.use(express.json());
 
-// Sử dụng routes
-// Tất cả các route trong sensorRoutes sẽ có tiền tố /api
-// Ví dụ: GET /api/sensordata
-app.use('/api/sensors', sensorRoutes); // API cho cảm biến
-app.use('/api/devices', deviceRoutes); // API cho thiết bị
-app.use('/api', userRoutes); // API cho user (sẽ khớp với /api/profile)
 
-// Chạy hàm main
+app.use('/api/sensors', sensorRoutes); 
+app.use('/api/devices', deviceRoutes);
+app.use('/api', userRoutes); 
+
+
 main();
